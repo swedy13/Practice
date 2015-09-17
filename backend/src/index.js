6b256aa2351d20c23baade9bd22e9d1907dc4ce6
Mark Swedberg <swedy13@gmail.com>
2015-09-17 16:04:47 -0500

    Removed old practice folders

--- backend/src/index.js
+++
@@ -1,6 +0,0 @@
-import t from 'transducers.js';
-
-export default function(req, res) {
-  var arr = JSON.parse(req.query.arr || '[]');
-  res.send(t.map(arr, function(x) { return x + 1; }));
-};
--- refresh/components/private.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var component = require('./base/component.js'),
-	css = require('./main.styl');
-
-
-document.body.appendChild(component());
